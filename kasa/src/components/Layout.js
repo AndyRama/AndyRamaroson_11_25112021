e21aeb8b7e2e14b4cfebad81b61a09b0f12aa65a
AndyRama <andyramaroson@gmail.com>
2022-09-12 14:52:49 +0200

    [fix] Review code for home

--- kasa/src/components/Layout.js
+++ kasa/src/components/Layout.js
@@ -1,26 +1,28 @@
-import { Routes, Route  } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 //components Header && Footer
 import Navbar from 'components/Navbar/Navbar';
-// import Banner from 'components/Banner/Banner';
-// import Footer from 'components/Footer/Footer';
+import Banner from 'components/Banner/Banner';
+import Footer from 'components/Footer/Footer';
 
 import Home from 'pages/Home/Home';
-// import About from 'pages/About/About';
+import About from 'pages/About/About';
 import PageNotFound from "pages/PageNotFound/PageNotFound";
 
 function Layout() {
   return (
     <div className="Layout">
       <Navbar />
-      {/* <Banner /> */}
+      <Banner />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
-        <PageNotFound/>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );  
 }
+
 
 
 export default Layout;
---
+++ kasa/src/pages/About/About.js
@@ -0,0 +1,31 @@
+// import './About.scss'
+// // import { Link } from "react-router-dom";
+// import { FaAngleDown} from "./assets/FaAngleDown.png"
+// import { FaAngleUp } from "./assets/FaAngleUp.png"
+
+// function About() {
+//   const isExpanded = this.state.expand;
+
+//   return (
+//     <section>
+//       <div className="cover-photo-about"></div>
+//       <ul className="list-wrapper">
+//         <li>
+//           <div className="list-item">
+//             <p>Fiabiliiter</p>
+//             <div className="icon-wrrapper">
+//               {isExpanded ? < FaAngleDown  size="24px" color="white"/> : < FaAngleUp  size="24px" color="white"/>}
+//             </div>
+//             <div className={ isExpanded ? "collapsed-text" : "expanded-text" }>
+//               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In fugiat odit, accusantium eligendi delectus tenetur. Obcaecati asperiores eius cupiditate
+//                  assumenda minus reprehenderit doloremque, facere praesentium fuga porro debitis optio voluptas!
+//               </p>
+//             </div>
+//           </div>
+//         </li>
+//       </ul>
+//     </section>
+//   ) 
+// }
+
+// export default About;
