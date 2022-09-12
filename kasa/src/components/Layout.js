96cc0423d2df39d569412e716e3362758e8e4fa2
AndyRama <andyramaroson@gmail.com>
2022-09-12 11:44:00 +0200

    [ehancement] add PAGE 404 and scss Mobile

--- kasa/src/components/Layout.js
+++ kasa/src/components/Layout.js
@@ -1,22 +1,23 @@
 import { Routes, Route  } from "react-router-dom";
 //components Header && Footer
 import Navbar from 'components/Navbar/Navbar';
-import Banner from 'components/Banner/Banner';
-import Footer from 'components/Footer/Footer';
+// import Banner from 'components/Banner/Banner';
+// import Footer from 'components/Footer/Footer';
 
 import Home from 'pages/Home/Home';
 // import About from 'pages/About/About';
-// import PageNotFound from "pages/PageNotFound/PageNotFound";
+import PageNotFound from "pages/PageNotFound/PageNotFound";
 
 function Layout() {
   return (
     <div className="Layout">
       <Navbar />
-      <Banner />
+      {/* <Banner /> */}
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-      <Footer />
+        <PageNotFound/>
+      {/* <Footer /> */}
     </div>
   );  
 }
